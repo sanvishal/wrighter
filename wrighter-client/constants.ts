ffd22e21d029da507ff51bf9f2f77a7cf39a44dd
Vishal TK <vishalsanthosh1999@gmail.com>
2022-07-23 12:14:21 +0530

    add env

--- wrighter-client/constants.ts
+++ wrighter-client/constants.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080/api";
--- wrighter-client/next.config.js
+++ wrighter-client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
