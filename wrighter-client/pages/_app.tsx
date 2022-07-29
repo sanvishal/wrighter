fb53b228c79303288051d0e3aed26bec08407768
Vishal TK <vishalsanthosh1999@gmail.com>
2022-07-29 14:57:55 +0530

    ga change

--- wrighter-client/pages/_app.tsx
+++ wrighter-client/pages/_app.tsx
@@ -29,17 +29,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
 
       <Script strategy="lazyOnload" id="ga">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
                     page_path: window.location.pathname,
                     });
                 `}
